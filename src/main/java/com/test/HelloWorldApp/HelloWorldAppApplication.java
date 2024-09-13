39bb1030e8a8ea6ab573815046389b7ddaf99547
mahabaleshwars <147705296+mahabaleshwars@users.noreply.github.com>
2024-09-13 15:21:43 +0530

    second commit

--- src/main/java/com/test/HelloWorldApp/HelloWorldAppApplication.java
+++ src/main/java/com/test/HelloWorldApp/HelloWorldAppApplication.java
@@ -8,6 +8,7 @@ public class HelloWorldAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldAppApplication.class, args);
+		System.out.println("Hello, World!");
 	}
 
 }
